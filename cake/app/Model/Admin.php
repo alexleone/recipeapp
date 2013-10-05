70964db68e999aa9ae130412ada02d0a076d6a2d
Joseph Lorence <ait390s1303jlorence@gmu.edu>
2013-10-05 15:04:08 -0400

    Added schema and validation

--- cake/app/Model/Admin.php
+++ cake/app/Model/Admin.php
@@ -4,7 +4,39 @@ App::uses('Admin', 'AppModel');
 
 
 class AdminModel extends AppModel {
-
+	
+	var $useTable = false;
+    var $_schema = array(
+        'item_id'		=>array('type'=>'int', 'length'=>7), 
+        'item_name'		=>array('type'=>'string', 'length'=>100), 
+        'item_description'	=>array('type'=>'string' 'length'=>150),
+        'item_category' =>array('type'=>'string' 'length'=>100),
+        'item_image' =>array('type'=>'string' 'length'=>150),
+        'pricing' =>array('type'=>'double' 'length'=>10)
+        
+    );
+    
+    var $validate = array(
+    'item_id' => array(
+        'rule'=>array('minLength', 5), 
+        'message'=>'Item ID must be 5 digits' ),
+    'item_name' => array(
+        'rule'=>'minlength' 1, 
+        'message'=>'Must have a name' ),
+    'item_description' => array(
+        'rule'=>array('minLength', 1), 
+        'message'=>'Description is required' ),
+    'item_category' =>array(
+    	'rule'=>array('minLength', 1),
+    	'message'=>'Category is required'
+    'item_image' =>array(
+    	'rule'=>array('minLength', 1),
+    	'message'=>'Image is required'
+    'pricing' =>array(
+    	'rule'=>array('minLength', 1),
+    	'message'=>'Price is required')
+    
+	);
 
 	public function getItem($item){
 		// API credentials
